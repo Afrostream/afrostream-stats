dbbd0f4fccbb7cbae48c715737b543c12d634882
Marc Dassonneville <marc.dassonneville@gmail.com>
2017-04-12 10:41:49 +0200

    metabase: key generation

--- AWS/self-generation3/key_generation.sh
+++ AWS/self-generation3/key_generation.sh
@@ -5,3 +5,6 @@ openssl genrsa 2048 > privatekey.pem
 openssl req -new -key privatekey.pem -out csr.pem
 
 
+# http://stackoverflow.com/questions/18283507/elasticbeanstalk-ssl-without-custom-domain
+openssl x509 -req -days 365 -in csr.pem -signkey privatekey.pem -out server.crt
+
